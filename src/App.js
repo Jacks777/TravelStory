2eecb61e311e18f89132bc54a3bcff0854101f38
Jacks777 <jools.l.reinartz@gmail.com>
2024-01-03 21:24:02 +0100

    Authentication commit
    Made a very simple authentication system using firebase

--- src/App.js
+++ src/App.js
@@ -1,16 +1,30 @@
 import { useState } from "react";
 import "./App.css";
 
+import {
+  ref,
+  get,
+  push,
+  set,
+  orderByChild,
+  equalTo,
+  query,
+} from "firebase/database";
+import { database } from "./config";
+
 function App() {
+  const [isLoggedIn, setIsLoggedIn] = useState("");
+
   return (
     <div className="App">
-      <Intro />
+      {!isLoggedIn ? <Intro setIsLoggedIn={setIsLoggedIn} /> : <MainPage />}
     </div>
   );
 }
 
-function Intro() {
+function Intro({ setIsLoggedIn }) {
   const [authOpen, setAuthOpen] = useState(false);
+  const [error, setError] = useState("");
 
   const handleOpenAuth = () => {
     setAuthOpen(!authOpen);
@@ -26,6 +40,9 @@ function Intro() {
 
   return (
     <div className="auth_container">
+      <div className={`error_message ${error ? "active" : ""}`}>
+        <div>{error}</div>
+      </div>
       <div className={`${authOpen ? "blur" : ""}`}>
         <video className="background_video" autoPlay loop muted playsInline>
           <source src="/assets/background/bg.mp4" type="video/mp4" />
@@ -40,7 +57,11 @@ function Intro() {
         <h2>Stories.</h2>
       </div>
       <div className={`auth_container-end ${authOpen && "auth"}`}>
-        {authOpen ? <Auth /> : <IntroMessage handleOpenAuth={handleOpenAuth} />}
+        {authOpen ? (
+          <Auth setIsLoggedIn={setIsLoggedIn} setError={setError} />
+        ) : (
+          <IntroMessage handleOpenAuth={handleOpenAuth} />
+        )}
       </div>
     </div>
   );
@@ -56,33 +77,199 @@ function IntroMessage({ handleOpenAuth }) {
   );
 }
 
-function Auth() {
+function Auth({ setError, setIsLoggedIn }) {
   const [login, setLogin] = useState(true);
+  const [username, setUsername] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleLogin = async () => {
+    const usersRef = ref(database, "users");
+
+    try {
+      const snapshot = await get(usersRef);
+
+      if (snapshot.exists()) {
+        let credentialsMatch = false;
+
+        snapshot.forEach((userSnapshot) => {
+          const userData = userSnapshot.val();
+          const userEmail = userData.email;
+          const userPasswordHash = userData.password;
+
+          if (
+            userEmail === email &&
+            hashPassword(password) === userPasswordHash
+          ) {
+            credentialsMatch = true;
+            const userKey = userSnapshot.key;
+            console.log(
+              "Credentials Match Found - User Key:",
+              userKey,
+              "User Data:",
+              userData
+            );
+            setIsLoggedIn(userKey);
+          }
+        });
+
+        if (!credentialsMatch) {
+          setError("Email or password is incorrect.");
+          console.log(
+            "Email or password does not match any user in the database"
+          );
+        }
+      } else {
+        setError("No users found.");
+        console.log("No users found");
+      }
+    } catch (error) {
+      console.error("Error checking credentials: ", error);
+    }
+  };
+
+  const handleRegister = async () => {
+    const usersRef = ref(database, "users");
+
+    const validateEmail = (email) => {
+      // Regular expression for a basic email validation
+      const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+      return emailRegex.test(email);
+    };
+
+    if (email.length < 1 || !validateEmail(email)) {
+      setError("Please enter a valid email address.");
+      return;
+    }
+
+    if (password.length < 6 || password.length > 48) {
+      setError(
+        "Password should contain at least 6 characters, and not more than 48"
+      );
+      return;
+    }
+
+    if (username.length < 3 || username.length > 24) {
+      setError(
+        "Username should contain at least 3 characters, and not more than 24."
+      );
+      return;
+    }
+
+    try {
+      const snapshot = await get(usersRef);
+
+      if (snapshot.exists()) {
+        let emailUsed = false;
+        snapshot.forEach((userSnapshot) => {
+          const userData = userSnapshot.val();
+          const userEmail = userData.email;
+
+          if (userEmail === email) {
+            emailUsed = true;
+            setError("Email is already taken");
+            return;
+          }
+        });
+
+        if (emailUsed) {
+          setError("Email is already taken");
+          console.log("Email already taken.");
+          return;
+        }
+      } else {
+      }
+    } catch (error) {
+      console.error("Error checking credentials: ", error);
+    }
+
+    try {
+      const hashedPassword = hashPassword(password);
+
+      const newUserData = {
+        username,
+        email,
+        password: hashedPassword,
+      };
+
+      const newPushRef = push(usersRef);
+      await set(newPushRef, newUserData);
+
+      console.log("Data pushed successfully with key: ", newPushRef.key);
+
+      setLogin(true);
+    } catch (error) {
+      console.error("Error pushing data: ", error);
+    }
+  };
+
+  const hashPassword = (password) => {
+    let hashedPassword = 0;
+    for (let i = 0; i < password.length; i++) {
+      const charCode = password.charCodeAt(i);
+      hashedPassword = (hashedPassword << 5) - hashedPassword + charCode;
+    }
+
+    return String(hashedPassword);
+  };
+
   return (
     <>
       {login ? (
         <>
           <label>Email</label>
-          <input type="email" />
+          <input
+            placeholder="Please enter your email"
+            onChange={(e) => setEmail(e.target.value)}
+            type="email"
+          />
           <label>Password</label>
-          <input type="password" />
-          <h3>Login</h3>
+          <input
+            placeholder="Please enter your password"
+            onChange={(e) => setPassword(e.target.value)}
+            type="password"
+          />
+          <h3 onClick={handleLogin}>Log in</h3>
           <p onClick={() => setLogin(!login)}>or Register</p>
         </>
       ) : (
         <>
           <label>Username</label>
-          <input type="name" />
+          <input
+            placeholder="Atleast 3 characters"
+            onChange={(e) => setUsername(e.target.value)}
+            type="text"
+          />
           <label>Email</label>
-          <input type="email" />
+          <input
+            placeholder="Please enter a valid email"
+            onChange={(e) => setEmail(e.target.value)}
+            type="email"
+          />
           <label>Password</label>
-          <input type="password" />
-          <h3>Register</h3>
-          <p onClick={() => setLogin(!login)}>or Login</p>
+          <input
+            placeholder="Atleast 6 characters"
+            onChange={(e) => setPassword(e.target.value)}
+            type="password"
+          />{" "}
+          <h3 onClick={handleRegister}>Register</h3>
+          <p
+            onClick={() => {
+              setLogin(!login);
+              setError("");
+            }}
+          >
+            or Login
+          </p>
         </>
       )}
     </>
   );
+}
+
+function MainPage() {
+  return <div>Yes</div>;
 }
 
 export default App;
--- src/config.js
+++ src/config.js
@@ -1,9 +1,6 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { getDatabase } from "firebase/database";
 
-// Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: "AIzaSyAoRZer7mAiQ2p2YMPJhQ0_LIEPic4pti0",
   authDomain: "travelstory-db05b.firebaseapp.com",
@@ -11,7 +8,11 @@ const firebaseConfig = {
   storageBucket: "travelstory-db05b.appspot.com",
   messagingSenderId: "486855249372",
   appId: "1:486855249372:web:11316f0f26efa8b843d319",
+  databaseURL:
+    "https://travelstory-db05b-default-rtdb.europe-west1.firebasedatabase.app",
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
+const database = getDatabase(app);
+
+export { database };
