23c8e6ccb6337ee31505d95eb6240694a9eccf1b
Jacks777 <jools.l.reinartz@gmail.com>
2024-01-16 13:53:37 +0100

    Finall Update

--- src/config.js
+++ src/config.js
@@ -3,14 +3,13 @@ import { getDatabase } from "firebase/database";
 import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAoRZer7mAiQ2p2YMPJhQ0_LIEPic4pti0",
-  authDomain: "travelstory-db05b.firebaseapp.com",
-  projectId: "travelstory-db05b",
-  storageBucket: "travelstory-db05b.appspot.com",
-  messagingSenderId: "486855249372",
-  appId: "1:486855249372:web:11316f0f26efa8b843d319",
-  databaseURL:
-    "https://travelstory-db05b-default-rtdb.europe-west1.firebasedatabase.app",
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
 };
 
 const app = initializeApp(firebaseConfig);
